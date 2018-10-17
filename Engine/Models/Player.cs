c33e41a496bef987024f532b578ef98340dae0fe
Web Rebel <web_rebel@outlook.com>
2018-10-17 05:20:28 +0100

    Created the Player Class

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Engine.Models
+{
+    class Player
+    {
+        string Name { get; set; }
+        string CharacterClass { get; set; }
+        int HitPoints { get; set; }
+        int ExperiencePoints { get; set; }
+        int Level { get; set; }
+        int Gold { get; set; }
+    }
+}

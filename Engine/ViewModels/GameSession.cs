025f61759882a39aebca1e8b3875d640f893eec7
Web Rebel <web_rebel@outlook.com>
2018-10-21 09:56:07 +0100

    Created the GameSession (ViewModel) Class

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,25 @@
+﻿
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            /**
+             * This is a test player object.  
+             * The player will be loaded from disk or other in future
+             */
+            CurrentPlayer.Name = "Richard";
+            CurrentPlayer.CharacterClass = "Wizard";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+            CurrentPlayer.Gold = 10;
+
+        }
+    }
+}

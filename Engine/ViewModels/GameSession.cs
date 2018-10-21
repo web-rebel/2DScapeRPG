4b36051e627cb81248c2c802d40fb0f8f427d9ec
Web Rebel <web_rebel@outlook.com>
2018-10-21 11:26:22 +0100

    Displaying the Player Object

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -3,9 +3,9 @@ using Engine.Models;
 
 namespace Engine.ViewModels
 {
-    class GameSession
+    public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
 
         public GameSession()
         {
@@ -13,12 +13,15 @@ namespace Engine.ViewModels
              * This is a test player object.  
              * The player will be loaded from disk or other in future
              */
-            CurrentPlayer.Name = "Richard";
-            CurrentPlayer.CharacterClass = "Wizard";
-            CurrentPlayer.HitPoints = 10;
-            CurrentPlayer.ExperiencePoints = 0;
-            CurrentPlayer.Level = 1;
-            CurrentPlayer.Gold = 10;
+            CurrentPlayer = new Player
+            {
+                Name = "Richard",
+                CharacterClass = "Wizard",
+                HitPoints = 10,
+                ExperiencePoints = 0,
+                Level = 1,
+                Gold = 10
+            };
 
         }
     }

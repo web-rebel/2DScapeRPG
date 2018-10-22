c64a144350c09e2adf268a50c165f664a62975c7
Web Rebel <web_rebel@outlook.com>
2018-10-22 17:10:28 +0100

    Created Game Items

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,33 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Factories
+{
+    public static class ItemFactory
+    {
+        private static List<GameItem> _standardGameItems;
+
+        static ItemFactory()
+        {
+            _standardGameItems = new List<GameItem>();
+            _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
+            _standardGameItems.Add(new Weapon(1002, "Wooden Short Sword", 3, 2, 3));
+        }
+
+        public static GameItem CreateItem(int itemTypeID)
+        {
+            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
+
+            if (standardItem != null)
+            {
+                return standardItem.Clone();
+            }
+
+            return null;
+        }
+    }
+}

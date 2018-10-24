d4b96c50aa627ca293bea217ccb839fae47b6acf
Web Rebel <web_rebel@outlook.com>
2018-10-24 11:21:03 +0100

    Created Monsters

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -16,8 +16,12 @@ namespace Engine.Factories
             _standardGameItems = new List<GameItem>();
             _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
             _standardGameItems.Add(new Weapon(1002, "Wooden Short Sword", 3, 1, 3));
-            _standardGameItems.Add(new GameItem(9001, "Sanke Fang", 1));
+            _standardGameItems.Add(new GameItem(9001, "Snake fang", 1));
             _standardGameItems.Add(new GameItem(9002, "Snakeskin", 2));
+            _standardGameItems.Add(new GameItem(9003, "Rat tail", 1));
+            _standardGameItems.Add(new GameItem(9004, "Rat fur", 2));
+            _standardGameItems.Add(new GameItem(9005, "Spider fang", 1));
+            _standardGameItems.Add(new GameItem(9006, "Spider silk", 2));
         }
 
         public static GameItem CreateItem(int itemTypeID)
